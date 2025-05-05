6697c3f5e61936a66849de8be6958c79ce27f108
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-05 21:41:35 +0300

    reorganize user routes to include authentication and password management endpoints

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,16 @@
-import {Router} from "express";
-import { signup,signin,getAllUsers,getUser,updateUser,deleteUser} from "../controllers/user.conroller.js";
-const router = Router();
-router.post('/signup',signup);
-router.post('/signin',signin);
-router.get('/',getAllUsers);
-router.get('/:id',getUser);
-router.put('/:id',updateUser);
-router.delete('/:id',deleteUser);
+import express from "express";
+import { checkAuth, forgotPassword, login, logout, resetPassword, signup, updateProfile, verifyEmail } from "../controllers/user.conroller.js";
+import { isAuthenticated } from "../middlewares/isAuthenticated.js";
+
+const router = express.Router();
+
+router.route("/check-auth").get(isAuthenticated, checkAuth);
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+router.route("/verify-email").post(verifyEmail);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:token").post(resetPassword);
+router.route("/profile/update").put(isAuthenticated,updateProfile);
 
 export default router;
