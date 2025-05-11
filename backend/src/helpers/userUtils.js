7d14ace1d9d5e490888c4a6477b83e943ca021af
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-12 01:23:47 +0300

    move backend code one step back

--- backend/src/helpers/userUtils.js
+++
@@ -1,7 +0,0 @@
-// Helper function for validation
-const validateEmail = (email) => {
-    const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email);
-  };
-
-  export {validateEmail};
