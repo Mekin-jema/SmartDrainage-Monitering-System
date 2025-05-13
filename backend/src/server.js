929f19da06a694771f87ecba2b3fcaa79b779438
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-13 18:01:19 +0300

    update CORS configuration to allow requests from any origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -92,7 +92,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Set this to your frontend's URL
+    origin: "*", // Set this to your frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow credentials like cookies or HTTP authentication
