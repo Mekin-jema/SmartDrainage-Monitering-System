ed12ce278a792c47ef121189d4e24abda8155af8
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-14 16:28:26 +0300

    fix: update CORS origin to production URL

--- backend/app.js
+++ backend/app.js
@@ -96,7 +96,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "https://final-project-v1-8lce.vercel.app", // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
