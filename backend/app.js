a2cc29706c38826d7ad81fb20fdab24b87b00804
Mekin Jemal <145870510+Mekin-jema@users.noreply.github.com>
2025-05-14 12:33:50 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://final-project-v1-cajg.vercel.app",
+    origin: "https://final-project-v1-8lce.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
