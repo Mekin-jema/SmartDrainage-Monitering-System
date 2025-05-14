bb417a8a2048ba77f0e1d72671c884878615cac9
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-14 09:54:47 +0300

    fix: remove trailing slash from CORS origin URL

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://final-project-v1-cajg.vercel.app/",
+    origin: "https://final-project-v1-cajg.vercel.app",
 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
