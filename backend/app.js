6d07aefeb13e68fce9609985db6753581ad3ddd1
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-14 09:54:24 +0300

    fix: update CORS configuration to use a single origin for production

--- backend/app.js
+++ backend/app.js
@@ -74,10 +74,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "https://final-project-v1-cajg.vercel.app",
-      "http://localhost:3000/",
-    ],
+    origin: "https://final-project-v1-cajg.vercel.app/",
+
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
