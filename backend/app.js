bb4c7da411e504d042366623821651d053b88b04
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-14 14:45:32 +0300

    update CORS origin to localhost for local development

--- backend/app.js
+++ backend/app.js
@@ -96,7 +96,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://final-project-v1-8lce.vercel.app",
+    origin: "http://localhost:5173", // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
