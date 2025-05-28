b8d77055bffab2c2feff73475c797048073515b1
Mekin Jemal <mekinjemal999@gmail.com>
2025-05-28 09:17:48 +0300

    add toast functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,18 +5,19 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store/Store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "@/pages/dashboard/theme-provider";
-import { Toaster } from "./components/ui/toaster";
+// import { Toaster } from "./components/ui/toaster";
 import Socket from "./socket/socket.io";
-
+import { Toaster } from 'sonner';
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ThemeProvider storageKey="vite-ui-theme">
 
-          <App />
-          <Toaster/>
-         <Socket/>
-    
+        <App />
+        {/* <Toaster/> */}
+        <Toaster richColors position="top-center" />
+        <Socket />
+
       </ThemeProvider>
     </PersistGate>
   </Provider>
